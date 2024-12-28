bfbfaa1cddbc11f8b87f63ffae690071207425fe
danetrov <danetrov@ya.ru>
2024-12-28 19:45:45 +0300

    add_list.py

--- add_list.py
+++ add_list.py
@@ -1,5 +1,5 @@
 main = input("Основная тема: ")
-title = input("Заголовок: ")
+headline = input("Заголовок: ")
 subtitle = input("Подзаголовок: ")
 titles = [main.upper(), title.title(), subtitle.title() ]
 print(titles)
