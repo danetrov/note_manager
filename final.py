f05cca3b6bf2b508e62c413970d721fd6fdbed41
danetrov <danetrov@ya.ru>
2025-01-04 16:59:37 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,15 @@
+username = input("Имя пользователя: ")
+title = input("Заголовок заметки: ")
+content = input("Описание заметки: ")
+status = input("Статус заметки: ")
+temp_created_date = input("Дата создания заметки (dd-mm-yyyy): ")
+created_date = (temp_created_date[:5])
+temp_issue_date = input("Дата истечения заметки (dd-mm-yyyy): ")
+issue_date = (temp_issue_date[:5])
+main = input("Основная тема: ")
+headline = input("Заголовок: ")
+subtitle = input("Подзаголовок: ")
+
+spisok = [username, title, [main, headline, subtitle],
+          content, status, created_date, issue_date]
+print(spisok)
