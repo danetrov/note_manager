c0782f4070a96a3f7cee99175362bb8ea7bbe832
danetrov <danetrov@ya.ru>
2025-01-04 16:57:30 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,9 @@
+temp_created_date = input('Дата создания заметки: ')
+created_date = (temp_created_date[:5])
+print(temp_created_date)
+print(created_date)
+
+temp_issue_date = input('Дата истечения заметки: ')
+issue_date = (temp_issue_date[:5])
+print(temp_issue_date)
+print(issue_date)
