2531f16dea490b2405b38972d86c4622608ad62f
danetrov <danetrov@ya.ru>
2025-01-04 16:52:43 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Имя пользователя: "
+print(username)
+title = "Заголовок заметки: "
+print(title)
+content = "Описание заметки: "
+print(content)
+status = "Статус заметки: "
+print(status)
+created_date = "Дата создания заметки (dd-mm-yyyy): "
+print(created_date)
+issue_date = "Дата истечения заметки (dd-mm-yyyy): "
+print(issue_date)
