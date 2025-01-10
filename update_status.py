69af2c13460c7d63f6eb33923b2c6a6620c5c0b7
danetrov <danetrov@ya.ru>
2025-01-10 18:54:52 +0300

    update_status.py

--- update_status.py
+++ update_status.py
@@ -5,15 +5,15 @@ print("Текущий статус заметки: " + current_status)
 
 print("\nВыберите новый статус заметки:")
 for i, status in enumerate(statuses, start=1):
-        print(f"{i}. {status}")
+        print(f"{i}. {status}") #выводим последовательный список статусов на выбор
 while True:
     try:
         choice = int(input("\nВаш выбор: "))
-        if  1 <= choice <= len(statuses):
+        if  1 <= choice <= len(statuses): # 1 условие для верного ответа который выводится
             current_status = statuses[choice -1]
             print("Статус заметки успешно обновлён на: " + current_status)
             break
         else:
             print("Выберите номер из списка(от 1 до 3)")
-    except ValueError:
+    except ValueError: # 2 условие если человек введет не число
         print("Некорректный ввод. Пожалуйста, введите число.")
