d527a2c7fbccf63ea137465f988825a8ce7cd162
danetrov <danetrov@ya.ru>
2025-01-15 20:50:36 +0300

    multiple_notes.py

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,76 +1,64 @@
 # Работа с несколькими заметками
-
 print("Добро пожаловать в 'Менеджер заметок'!")
-
-notes = []
-while True: # Основной бесконечный цикл
+notes = {}
+note_id = 1
+while True:
     qwe = input("Хотите добавить новую заметку? (да/нет): ")
 
-    if qwe == "да" or qwe == "lf" or qwe == "LF" or qwe == "ДА" or qwe == "Да" or qwe == "Lf":
+    if qwe.lower() in ["да", "lf"]:
         username = input("Имя пользователя: ")
-        while True:
-            if username == "":
-                print("Пожалуйста укажите имя")
-                username = input("Имя пользователя: ")
-            else:
-                break
+        while not username:
+            print("Пожалуйста укажите имя")
+            username = input("Имя пользователя: ")
+
         title = input("Заголовок заметки: ")
-        while True:
-            if title == "":
-                print("Пожалуйста укажите заголовок")
-                title = input("Заголовок заметки: ")
-            else:
-                break
+        while not title:
+            print("Пожалуйста укажите заголовок")
+            title = input("Заголовок заметки: ")
+
         content = input("Описание заметки: ")
-        while True:
-            if content == "":
-                print("Пожалуйста укажите описание заметки")
-                content = input("Описание заметки: ")
-            else:
-                break
+        while not content:
+            print("Пожалуйста укажите описание заметки")
+            content = input("Описание заметки: ")
+
         status = input("Статус заметки: ")
-        while True:
-            if status == "":
-                print("Пожалуйста укажите статус заметки")
-                status = input("Статус заметки: ")
-            else:
-                break
+        while not status:
+            print("Пожалуйста укажите статус заметки")
+            status = input("Статус заметки: ")
+
         created_date = input("Дата создания заметки (dd-mm-yyyy): ")
-        while True:
-            if created_date == "":
-                print("Пожалуйста укажите дату создания заметки (dd-mm-yyyy)")
-                created_date = input("Дата создания заметки (dd-mm-yyyy): ")
-            else:
-                break
+        while not created_date:
+            print("Пожалуйста укажите дату создания заметки (dd-mm-yyyy)")
+            created_date = input("Дата создания заметки (dd-mm-yyyy): ")
+
         issue_date = input("Дата истечения заметки (dd-mm-yyyy): ")
-        while True:
-            if issue_date == "":
-                print("Пожалуйста дату истечения заметки")
-                issue_date = input("Дата истечения заметки (dd-mm-yyyy): ")
-            else:
-                break
+        while not issue_date:
+            print("Пожалуйста укажите дату истечения заметки")
+            issue_date = input("Дата истечения заметки (dd-mm-yyyy): ")
 
-        notes.append(username)
-        notes.append(title)
-        notes.append(content)
-        notes.append(status)
-        notes.append(created_date)
-        notes.append(issue_date)
+        notes[note_id] = {
+            "Имя": username,
+            "Заголовок": title,
+            "Описание": content,
+            "Статус": status,
+            "Дата создания": created_date,
+            "Дедлайн": issue_date
+        }
 
-    elif qwe == "нет" or qwe == "ytn" or qwe == "YTN" or qwe == "НЕТ" or qwe == "Нет" or qwe == "Ytn":
+        note_id += 1
+
+    elif qwe.lower() in ["нет", "ytn"]:
         break
     else:
         print("Не понимаю вас, ответьте пожалуйста да/нет")
 
 print("Вот что получилось:\n")
 
-for index, item in enumerate(notes):
-    number = (index % 6) + 1
-
-    if number == 1 and index != 0:
-        print()
-
-    print(f"{number}. {item}")
+for note_id, note in notes.items():
+    print(f"Заметка {note_id}:")
+    for key, value in note.items():
+        print(f"{key}: {value}")
+    print()
 
 
 
