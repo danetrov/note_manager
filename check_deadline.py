76e2d7e36cbed4e38c39986ea22383245ca2b8db
danetrov <danetrov@ya.ru>
2025-01-21 14:02:10 +0300

    check_deadline*.py

--- check_deadline.py
+++ check_deadline.py
@@ -3,34 +3,34 @@
 from datetime import datetime, date
 
 a = date.today()
-print("Текущая дата:", a.day,"-", a.month,"-", a.year) # Выводим текущую дату
+print("Текущая дата:", a.day, "-", a.month, "-", a.year)  # Выводим текущую дату
 
-dny = ["день", "дня", "дней"] #Список со склонением слово день для корректного вывода
+dny = ["день", "дня", "дней"]  # Список со склонением слово день для корректного вывода
 
-while True: #цикл для неправильного ответа пользователя
-    try: #блок для исключения ошибки в дате
+while True:  # цикл для неправильного ответа пользователя
+    try:  # блок для исключения ошибки в дате
 
-        issue_date = input("\nВведите дату дедлайна(yyyy-mm-dd): ")
-        issue_date = datetime.strptime(issue_date, "%Y-%m-%d") #Преобразуем формат
-        issue_date = issue_date.date() #команда для корректной работы сравнения дат,преобразуем datetime -> date
+        issue_date = input("\nВведите дату дедлайна(dd-mm-yyyy): ")
+        issue_date = datetime.strptime(issue_date, "%d-%m-%Y").date()  # Преобразуем формат(Исправил)
 
-        diff = (issue_date - a) #Разница дат
+        diff = (issue_date - a)
 
         if diff.days > 0:
             if diff.days == 1:
-                print("До дедлайна осталось: ", diff.days, dny[0])
-            elif diff.days == 2 or diff.days == 3 or diff.days == 4:
-                print("До дедлайна осталось: ", diff.days, dny[1])
+                print("До дедлайна осталось:", diff.days, dny[0])
+            elif diff.days in (2, 3, 4): # Исправил
+                print("До дедлайна осталось:", diff.days, dny[1])
             else:
-                print("До дедлайна осталось: ", diff.days, dny[2])
+                print("До дедлайна осталось:", diff.days, dny[2])
 
         elif diff.days < 0:
-            if -diff.days == 1:
-                print("Внимание! Дедлайн истек: ", -diff.days, dny[0], "назад")
-            elif -diff.days == 2 or -diff.days == 3 or -diff.days == 4:
-                print("Внимание! Дедлайн истек: ", -diff.days, dny[1], "назад")
+            diff = abs(diff) # Исправил
+            if diff.days == 1:
+                print("Внимание! Дедлайн истек:", diff.days, dny[0], "назад")
+            elif diff.days in (2, 3, 4):
+                print("Внимание! Дедлайн истек:", diff.days, dny[1], "назад")
             else:
-                print("Внимание! Дедлайн истек: ", -diff.days, dny[2], "назад")
+                print("Внимание! Дедлайн истек:", diff.days, dny[2], "назад")
 
         else:
             print("Дедлайн сегодня")
