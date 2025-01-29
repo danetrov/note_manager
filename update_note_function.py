29254002bd918f94ac5eec198b6b99125d42a3a9
danetrov <danetrov@ya.ru>
2025-01-29 17:53:20 +0300

    update_note_function.py

--- update_note_function.py
+++ update_note_function.py
@@ -1,4 +1,28 @@
-update_note = {
+def update_note(note):
+    print("Текущая заметка:")
+    for key, value in note.items():
+        print(f"{key}: {value}")
+
+    while True:
+
+        choice = input("\nВведите значение которое хотите поменять(или Enter для выхода): ")
+
+        if choice == "":
+            break
+
+        elif choice in note:
+            new_choice = input(f"Введите новое значение для {choice}: ")
+            note[choice] = new_choice
+
+        else:
+            print("Такого значения нет, Введите еще раз значение которое хотите поменять(например: username)")
+
+    print("Обновленная заметка:")
+    for key, value in note.items():
+        print(f"{key}: {value}")
+
+
+update_note_data = {
     'username': 'Алексей',
     'title': 'Список покупок',
     'content': 'Купить продукты на неделю',
@@ -6,24 +30,5 @@ update_note = {
     'created_date': '27-11-2024',
     'issue_date': '30-11-2024'
 }
-print("Текущая заметка:")
-for key, value in update_note.items():
-    print(f"{key}: {value}")
 
-while True:
-
-    choice = input("\nВведите значение которое хотите поменять(или Enter для выхода): ")
-
-    if choice == "":
-        break
-
-    elif choice in update_note:
-        new_choice = input(f"Введите новое значение для {choice}: ")
-        update_note[choice] = new_choice
-
-    else:
-        print("Такого значения нет, Введите еще раз значение которое хотите поменять(например: username)")
-
-print("Текущая заметка:")
-for key, value in update_note.items():
-    print(f"{key}: {value}")
+update_note(update_note_data)
